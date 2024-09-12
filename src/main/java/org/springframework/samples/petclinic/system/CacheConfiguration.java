d6986d598ed8122f99fc3a78c7619cff378cbdf8
ShizukaKurosawa <nkmr44yr@gmail.com>
2024-09-13 01:27:22 +0900

    エラー確認画面

---
+++ src/main/java/org/springframework/samples/petclinic/system/CacheConfiguration.java
@@ -0,0 +1,23 @@
+package org.springframework.samples.petclinic.system;
+
+import javax.cache.configuration.MutableConfiguration;
+
+import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+@EnableCaching
+public class CacheConfiguration {
+	
+	@Bean
+	public JCacheManagerCustomizer petclinicCacheConfigurationCostomizer() {
+		return cm -> cm.createCache("vets", cacheConfiguration());
+	}
+	
+	private javax.cache.configuration.Configuration<Object, Object> cacheConfiguration(){
+		return new MutableConfiguration<>().setStatisticsEnabled(true);
+	}
+
+}
