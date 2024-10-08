d72f593cf4b959b57e620e46474cfd6ddd88b6a1
ShizukaKurosawa <nkmr44yr@gmail.com>
2024-10-09 00:22:08 +0900

    処理コメント追記

--- src/main/java/org/springframework/samples/petclinic/system/CrashController.java
+++ src/main/java/org/springframework/samples/petclinic/system/CrashController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 class CrashController {
 	
+	// エラー確認画面を表示する処理
 	@GetMapping("/oups")
 	public String triggerException() {
+		// 何かしらのeExceptionが発生すると下記メッセージを格納する
 		throw new RuntimeException("Expected: controller used to showcase what " + "happens when an exception is thrown");
 	}
 
